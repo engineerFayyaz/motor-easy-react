51c6be2b1c0c086aae25a7a35ed6de10be1110d9
codesinc-repo <codes.inc.com@gmail.com>
2024-06-26 18:42:24 +0500

    Code updated |Fayyaz|Latest one

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from './Pages/Home';
 import AboutUs from './Pages/AboutUs';
 import Contactus from "./Pages/Contactus"
@@ -29,9 +30,11 @@ import AppointmentsData from './Pages/admin/BuyAppointements/Appointements';
 import AdminBanners from './Pages/admin/AdminBanners';
 
 
+
 function App() {
   return (
     <Router>
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/About-us" element={<AboutUs />} />
